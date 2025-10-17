aebb321bf565a386ca75d28624a34e0674d79abd
Harena-Git <faniriantsoaharena@gmail.com>
2025-10-17 02:24:07 +0100

    Mise en place

---
+++ Compile.bat
@@ -0,0 +1,19 @@
+# 1. Aller dans le dossier customer
+cd customer
+mvn clean install
+
+# 2. Aller dans le dossier current  
+cd ../current
+mvn clean install
+
+# 3. Aller dans le dossier loan
+cd ../loan
+mvn clean install
+
+# 4. Aller dans le dossier centralizer
+cd ../centralizer
+mvn clean install
+
+# 5. Aller dans le dossier Bank (EAR)
+cd ../Bank
+mvn clean install
